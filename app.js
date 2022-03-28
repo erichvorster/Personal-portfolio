a8ce3204ffb93835861f643308bdb33887862237
erichvorster <erichvorster1@gmail.com>
2022-03-28 22:50:18 +0200

    2nd commit

--- app.js
+++ app.js
@@ -28,6 +28,26 @@ const navSlide = () =>{
 
 navSlide()
 
+//MOUSE CIRCLES
+
+document.body.addEventListener('mousemove', event => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    gsap.set(".cursor",{
+      x:mouseX,
+      y:mouseY
+    })
+
+    gsap.to('.shape',{
+        x:mouseX,
+        y:mouseY,
+        stagger:-0.1
+    })
+})
+
+
+
 
 //TEXT ANIMATION
 
