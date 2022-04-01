98595e8ac467ef5b1f66f8149d771254fbd8d224
erichvorster <erichvorster1@gmail.com>
2022-04-01 20:15:00 +0200

    3rd commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-const navSlide = () =>{
+const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li')
@@ -13,7 +13,7 @@ const navSlide = () =>{
             if(link.style.animation){
                 link.style.animation ='';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 1.5}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + .5}s`;
             }
             console.log(index / 7);
         })
