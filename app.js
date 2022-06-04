fae3d4d9155e2bbf9c134961a4fde02223e36b89
erichvorster <erichvorster1@gmail.com>
2022-06-04 19:13:35 +0200

    7th commit

--- app.js
+++ app.js
@@ -117,7 +117,9 @@ gsap.from(".email", {
   delay: 2,
 });
 
-gsap.from(".about__grid", {
+///About
+
+gsap.from(".about__header", {
   scrollTrigger: ".about__grid",
   y: 20,
   opacity: 0,
@@ -125,13 +127,87 @@ gsap.from(".about__grid", {
   delay: 0.4,
   ease: "power4",
 });
+gsap.from(".about__content--1", {
+  scrollTrigger: ".about__grid",
+  y: 50,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.5,
+  ease: "power4",
+});
+gsap.from(".about__content--2", {
+  scrollTrigger: ".about__grid",
+  y: 50,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.6,
+  ease: "power4",
+});
+gsap.from(".technologies__header", {
+  scrollTrigger: ".about__grid",
+  y: 50,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.7,
+  ease: "power4",
+});
+gsap.from(".technologies__container", {
+  scrollTrigger: ".about__grid",
+  y: 50,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.8,
+  ease: "power4",
+});
+gsap.from(".about__image__container", {
+  scrollTrigger: ".about__grid",
+  opacity: 0,
+  duration: 0.3,
+  delay: 1.2,
+  ease: "power4",
+});
 
-gsap.from(".project__container", {
+///Projects
+
+gsap.from(".project__title", {
   scrollTrigger: ".project",
-  y: 20,
+  y: 50,
   opacity: 0,
   duration: 0.8,
   delay: 0.4,
+  ease: "power4",
+});
+
+gsap.from(".project-1", {
+  scrollTrigger: ".project",
+  y: 50,
+  opacity: 0,
+
+  delay: 0.5,
+  ease: "power4",
+});
+gsap.from(".project-2", {
+  scrollTrigger: ".project",
+  y: 50,
+  opacity: 0,
+
+  delay: 0.55,
+  ease: "power4",
+});
+gsap.from(".project-3", {
+  scrollTrigger: ".project",
+  y: 50,
+  opacity: 0,
+
+  delay: 0.6,
+  ease: "power4",
+});
+gsap.from(".project-4", {
+  scrollTrigger: ".project",
+  y: 50,
+  opacity: 0,
+
+  delay: 0.65,
   ease: "power4",
 });
 
@@ -140,6 +216,29 @@ gsap.from(".contact__container", {
   y: 20,
   opacity: 0,
   duration: 0.8,
-  delay: 0.4,
+  delay: 0.3,
   ease: "power4",
+});
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains("scroll-down")
+  ) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
 });
