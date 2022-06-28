8200b8570e0df5d5185f0a25b2b2075a3970d2d2
erichvorster <erichvorster1@gmail.com>
2022-06-28 20:42:41 +0200

    9th commit

--- app.js
+++ app.js
@@ -242,3 +242,47 @@ window.addEventListener("scroll", () => {
   }
   lastScroll = currentScroll;
 });
+
+//Form validation
+
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  // trim to remove the whitespaces
+  const usernameValue = name.value.trim();
+  const emailValue = email.value.trim();
+
+  if (usernameValue === "") {
+    setErrorFor(name, "Username cannot be blank");
+  } else {
+    setSuccessFor(name);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Not a valid email");
+  } else {
+    setSuccessFor(email);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  formControl.className = "form-control error";
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
